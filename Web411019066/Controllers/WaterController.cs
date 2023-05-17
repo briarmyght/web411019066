7d5a7712e738d3d264ab7189ebab3ee8f09767d8
陳希盈 <anna042174@gmail.com>
2023-05-17 19:48:53 +0800

    Water

--- Web411019066/Controllers/WaterController.cs
+++ Web411019066/Controllers/WaterController.cs
@@ -9,8 +9,17 @@ namespace Web411019066.Controllers
     public class WaterController : Controller
     {
         // GET: Water
-        public ActionResult Index()
+        public ActionResult Index(string name,float? weight)
         {
+            var result = "";
+
+            if (weight.HasValue)
+            {
+                result = weight * 30 + "cc";
+
+            }
+            ViewBag.name = name;
+            ViewBag.result = result;
             return View();
         }
     }
